005d1ac1483281bf407d7fe382e7a81fbbfbecbe
flyingfoxweb <erin@flyingfox.net.nz>
2017-06-07 01:36:06 +0000

    Created the toggleAll method which will provide the ability to make all todos completed or all todos uncompleted.

--- js/app.js
+++ js/app.js
@@ -33,5 +33,29 @@ var todoList = {
         var todo = this.todos[position];
         todo.completed = !todo.completed;
         this.displayTodos();
+    },
+    toggleAll: function() {
+        var completedTodos = 0;
+        var totalTodos = this.todos.length;
+        
+        //Get the number of completed todos
+        for(var i = 0; i < totalTodos; i++) {
+            if(this.todos[i].completed === true) {
+                completedTodos++;
+            }
+        }
+        
+        //if everything is true, make everything false
+        if(completedTodos === totalTodos) {
+            for(var i = 0; i < totalTodos; i++) {
+                this.todos[i].completed = false;
+            } 
+        } else {
+            //If everything isn't true, Make everything true
+            for(var i = 0; i < totalTodos; i++) {
+                this.todos[i].completed = true;
+            }
+        }
+        this.displayTodos();
     }
 };
